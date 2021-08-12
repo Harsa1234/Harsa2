83b1b45943fa6fe8710c74031d53d7cc1768e148
Harsa1234 <88840319+Harsa1234@users.noreply.github.com>
2021-08-12 22:35:45 +0530

    Delete Loops-q2.py

--- Loops-q2.py
+++
@@ -1,7 +0,0 @@
-x=input("Enter list of numbers separated by space:")
-List=x.split(' ')
-print("List: ",List)
-print("The positive numbers in List are: ")
-for i in List:
-    if int(i)>=0:
-        print(i,end=' ')
