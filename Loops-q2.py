62973df532cedb0f77e6f4dd55df900a11c19dd5
Harsa1234 <88840319+Harsa1234@users.noreply.github.com>
2021-08-12 21:30:38 +0530

    Add files via upload

---
+++ Loops-q2.py
@@ -0,0 +1,12 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+print("List1: ",list1)
+print("The positive numbers in List1 are: ")
+for i in list1:
+    if i>=0:
+        print(i,end=' ')
+print("\nList2: ",list2)
+print("The positive numbers in List2 are: ")
+for j in list2:
+    if j>=0:
+        print(j,end=' ')
